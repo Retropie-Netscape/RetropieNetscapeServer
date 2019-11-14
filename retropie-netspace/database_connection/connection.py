346f9620febbe3201b224b29991700982315b440
alynch14 <alynch14@students.towson.edu>
2019-11-14 18:47:29 -0500

    Started class refactor for db connection

--- retropie-netspace/database_connection/connection.py
+++ retropie-netspace/database_connection/connection.py
@@ -1,12 +1,15 @@
 import sqlite3
 
-def create_connection(db_file):
-    #Creates connection to the specified sqlite3 database
+class DatabaseConnection:
+    
+    def create_connection(db_file):
+        #Creates connection to the specified sqlite3 database
 
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
+        conn = None
+        try:
+            conn = sqlite3.connect(db_file)
+        except Error as e:
+            print(e)
 
-    return conn
+    def get_connection() -> str:
+        return 
---
+++ retropie-netspace/user_endpoints/create_user.py
@@ -0,0 +1,4 @@
+from database_connection import connection
+
+def create_new_user(conn: connection):
+    return None
